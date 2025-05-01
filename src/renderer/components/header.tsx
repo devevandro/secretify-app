acc7f6e3216cbf7616d10c8166ea1ce87c117256
devevandro <dev.evandro@gmail.com>
2025-04-30 22:31:46 -0300

    fix: update header background color for improved visibility

--- src/renderer/components/header.tsx
+++ src/renderer/components/header.tsx
@@ -13,7 +13,7 @@ export default function Header() {
 
   return (
     <header
-      className={`draggable pb-2 w-full bg-[#2a2626] border-gray-900 border-b shadow-sm sticky top-0 z-10`}
+      className={`draggable pb-2 w-full bg-[#141414] border-gray-900 border-b shadow-sm sticky top-0 z-10`}
     >
       <div className="container  mx-auto px-4 py-3" />
     </header>
