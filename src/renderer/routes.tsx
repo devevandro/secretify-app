4e6264756f9388d3a30cfa9a1c31c2368d2fc595
devevandro <dev.evandro@gmail.com>
2025-05-09 21:56:51 -0300

    refactor: update route imports to use screen components directly

--- src/renderer/routes.tsx
+++ src/renderer/routes.tsx
@@ -2,24 +2,24 @@ import { Route } from "react-router-dom";
 
 import { Router } from "lib/electron-router-dom";
 
-import LoginPage from "./screens/login/page";
-import VerifyPage from "./screens/verify/page";
-import Dashboard from "./screens/dashboard/page";
-import FavoritesPage from "./screens/favorites/page";
-import TrashPage from "./screens/trash/page";
-import SignupPage from "./screens/signup/page";
+import LoginScreen from "./screens/login";
+import VerifyScreen from "./screens/verify";
+import Dashboard from "./screens/dashboard";
+import FavoritesScreen from "./screens/favorites";
+import TrashScreen from "./screens/trash";
+import SignupScreen from "./screens/signup";
 
 export function AppRoutes() {
   return (
     <Router
       main={
         <>
-          <Route path="/" element={<LoginPage />} />
-          <Route path="/verify" element={<VerifyPage />} />
+          <Route path="/" element={<LoginScreen />} />
+          <Route path="/verify" element={<VerifyScreen />} />
           <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/favorites" element={<FavoritesPage />} />
-          <Route path="/trash" element={<TrashPage />} />
-          <Route path="/signup" element={<SignupPage />} />
+          <Route path="/favorites" element={<FavoritesScreen />} />
+          <Route path="/trash" element={<TrashScreen />} />
+          <Route path="/signup" element={<SignupScreen />} />
         </>
       }
     />
