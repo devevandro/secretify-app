7aef7a06a95610d427e96c48ee76beceadf7536d
devevandro <dev.evandro@gmail.com>
2025-05-13 21:36:44 -0300

    refactor: standardize import statements and improve code formatting across setup, register-window-creation, and create modules

--- src/lib/electron-app/factories/app/setup.ts
+++ src/lib/electron-app/factories/app/setup.ts
@@ -1,54 +1,54 @@
-import { app, BrowserWindow } from 'electron'
+import { app, BrowserWindow } from "electron";
 
 import {
   installExtension,
   REACT_DEVELOPER_TOOLS,
-} from 'electron-extension-installer'
+} from "electron-extension-installer";
 
-import { ignoreConsoleWarnings } from '../../utils/ignore-console-warnings'
-import { PLATFORM, ENVIRONMENT } from 'shared/constants'
-import { makeAppId } from 'shared/utils'
+import { ignoreConsoleWarnings } from "../../utils/ignore-console-warnings";
+import { PLATFORM, ENVIRONMENTS } from "shared/constants/constants";
+import { makeAppId } from "shared/utils/utils";
 
-ignoreConsoleWarnings(['Manifest version 2 is deprecated'])
+ignoreConsoleWarnings(["Manifest version 2 is deprecated"]);
 
 export async function makeAppSetup(createWindow: () => Promise<BrowserWindow>) {
-  if (ENVIRONMENT.IS_DEV) {
+  if (ENVIRONMENTS.IS_DEV) {
     await installExtension([REACT_DEVELOPER_TOOLS], {
       loadExtensionOptions: {
         allowFileAccess: true,
       },
-    })
+    });
   }
 
-  let window = await createWindow()
+  let window = await createWindow();
 
-  app.on('activate', async () => {
-    const windows = BrowserWindow.getAllWindows()
+  app.on("activate", async () => {
+    const windows = BrowserWindow.getAllWindows();
 
     if (!windows.length) {
-      window = await createWindow()
+      window = await createWindow();
     } else {
       for (window of windows.reverse()) {
-        window.restore()
+        window.restore();
       }
     }
-  })
+  });
 
-  app.on('web-contents-created', (_, contents) =>
+  app.on("web-contents-created", (_, contents) =>
     contents.on(
-      'will-navigate',
-      (event, _) => !ENVIRONMENT.IS_DEV && event.preventDefault()
+      "will-navigate",
+      (event, _) => !ENVIRONMENTS.IS_DEV && event.preventDefault()
     )
-  )
+  );
 
-  app.on('window-all-closed', () => !PLATFORM.IS_MAC && app.quit())
+  app.on("window-all-closed", () => !PLATFORM.IS_MAC && app.quit());
 
-  return window
+  return window;
 }
 
-PLATFORM.IS_LINUX && app.disableHardwareAcceleration()
+PLATFORM.IS_LINUX && app.disableHardwareAcceleration();
 
 PLATFORM.IS_WINDOWS &&
-  app.setAppUserModelId(ENVIRONMENT.IS_DEV ? process.execPath : makeAppId())
+  app.setAppUserModelId(ENVIRONMENTS.IS_DEV ? process.execPath : makeAppId());
 
-app.commandLine.appendSwitch('force-color-profile', 'srgb')
+app.commandLine.appendSwitch("force-color-profile", "srgb");
