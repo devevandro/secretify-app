fbeefa82ace7f47973183bb74803d8e8c246f102
devevandro <dev.evandro@gmail.com>
2025-05-13 21:36:25 -0300

    feat: add constants and utility functions for environment and app ID generation

---
+++ src/shared/constants/constants.ts
@@ -0,0 +1,14 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+export const ENVIRONMENTS = {
+  IS_DEV: process.env.NODE_ENV === "development",
+  SECRETIFY_API: process.env.SECRETIFY_API,
+  SECRETIFY_API_KEY: process.env.SECRETIFY_API_KEY,
+};
+
+export const PLATFORM = {
+  IS_MAC: process.platform === "darwin",
+  IS_WINDOWS: process.platform === "win32",
+  IS_LINUX: process.platform === "linux",
+};
