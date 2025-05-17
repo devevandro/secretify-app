2d70f0517be9b210091ea9ea78b30ae0d55350c7
devevandro <dev.evandro@gmail.com>
2025-05-16 22:44:00 -0300

    feat: implement Favorites and Passwords screens with responsive design and data fetching

--- src/renderer/routes.tsx
+++ src/renderer/routes.tsx
@@ -8,6 +8,8 @@ import Dashboard from "./screens/dashboard";
 import FavoritesScreen from "./screens/favorites";
 import TrashScreen from "./screens/trash";
 import SignupScreen from "./screens/signup";
+import PasswordsScreen from "./screens/passwords";
+import FavoritesSitesScreen from "./screens/favorites-sites";
 
 export function AppRoutes() {
   return (
@@ -18,8 +20,10 @@ export function AppRoutes() {
           <Route path="/verify" element={<VerifyScreen />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/favorites" element={<FavoritesScreen />} />
+          <Route path="/favorites-sites" element={<FavoritesSitesScreen />} />
           <Route path="/trash" element={<TrashScreen />} />
           <Route path="/signup" element={<SignupScreen />} />
+          <Route path="/passwords" element={<PasswordsScreen />} />
         </>
       }
     />
