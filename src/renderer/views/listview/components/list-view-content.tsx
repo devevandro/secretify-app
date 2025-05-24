48a0a01ce162fbdbd65f93298a3126d616b95ec4
devevandro <dev.evandro@gmail.com>
2025-05-24 18:25:23 -0300

    feat: remove unused Item interface and related types for cleaner codebase

--- src/renderer/views/listview/components/list-view-content.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import type React from "react";
-
-export function ListViewContent({ children }: { children: React.ReactNode }) {
-  return <div className="flex flex-col space-y-2">{children}</div>;
-}
