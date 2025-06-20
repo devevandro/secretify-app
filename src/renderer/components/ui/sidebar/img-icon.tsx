dfad627726c0f793595f61602bc0a3faff9fd321
devevandro <dev.evandro@gmail.com>
2025-06-19 21:55:54 -0300

    feat: enhance ImgIcon component to support width, cursor, and onClick props

--- src/renderer/components/ui/sidebar/img-icon.tsx
+++ src/renderer/components/ui/sidebar/img-icon.tsx
@@ -1,5 +1,22 @@
 "use client";
 
-export function ImgIcon({ src }: { src: string }) {
-  return <img src={`/svg/${src}`} alt="" className="w-5" />;
+export function ImgIcon({
+  src,
+  width,
+  cursor,
+  onClick,
+}: {
+  src: string;
+  width?: string;
+  cursor?: string;
+  onClick?: () => void;
+}) {
+  return (
+    <img
+      src={`/svg/${src}`}
+      alt=""
+      className={`${width} ${cursor}`}
+      onClick={onClick}
+    />
+  );
 }
