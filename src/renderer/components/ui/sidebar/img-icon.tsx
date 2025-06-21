63e38953a83ab50ca203350a511f7c3a1ae9d537
devevandro <dev.evandro@gmail.com>
2025-06-21 18:36:59 -0300

    fix: wrap ImgIcon component in a button for improved click handling and accessibility

--- src/renderer/components/ui/sidebar/img-icon.tsx
+++ src/renderer/components/ui/sidebar/img-icon.tsx
@@ -4,19 +4,18 @@ export function ImgIcon({
   src,
   width,
   cursor,
+  disabled,
   onClick,
 }: {
   src: string;
   width?: string;
   cursor?: string;
+  disabled?: boolean;
   onClick?: () => void;
 }) {
   return (
-    <img
-      src={`/svg/${src}`}
-      alt=""
-      className={`${width} ${cursor}`}
-      onClick={onClick}
-    />
+    <button onClick={onClick} disabled={disabled}>
+      <img src={`/svg/${src}`} alt="" className={`${width} ${cursor}`} />
+    </button>
   );
 }
